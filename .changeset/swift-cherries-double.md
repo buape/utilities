413feb6e7eb026ce93a79bb356ce5c904ee744b5
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2023-07-15 19:35:36 -0700

    Fix cannot access BaseHandler before initialization (#74)
    * Fix cannot access BaseHandler before initialization
    
    * Fix the error in my ways
    
    * CHANGESETTTT TIMEEEEE

---
+++ .changeset/swift-cherries-double.md
@@ -0,0 +1,5 @@
+---
+"kiai.js": patch
+---
+
+Fix kiai.js to work in CJS by fixing an error
--- packages/kiai.js/src/handlers/index.ts
+++ packages/kiai.js/src/handlers/index.ts
@@ -1,14 +1,6 @@
-import { RequestHandler } from "../RequestHandler"
-
+export { BaseHandler } from "./BaseHandler"
 export { Blacklist } from "./Blacklist"
 export { Leveling } from "./Leveling"
 export { Multipliers } from "./Multipliers"
 export { Rewards } from "./Rewards"
 export { Settings } from "./Settings"
-
-export class BaseHandler {
-    public _handler: RequestHandler
-    constructor(handler: RequestHandler) {
-        this._handler = handler
-    }
-}
